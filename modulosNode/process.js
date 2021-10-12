7718ea96a866afcaffa8ca6546dd27299a125d69
Didier Irias Méndez <didier.irias@gmail.com>
2021-10-11 22:02:26 -0600

    Process Module

---
+++ modulosNode/process.js
@@ -0,0 +1,15 @@
+//const proceso = require('process'); Y aviene global
+
+process.on('exit', () => {
+    console.log('El proceso acabó');
+})
+
+process.on('beforeExit', () =>{
+    console.log('Apunto de terminar');
+})
+
+process.on('uncaughtException', (err, origen) => {
+    console.error('Se nos olvidó capturar un error');
+    console.error(err.message);
+})
+intentando();
